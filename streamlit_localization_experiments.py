4625c7fc7c3b87a5d5c9e57039d3e02ff786faf3
avokloti-streamlit <157151362+avokloti-streamlit@users.noreply.github.com>
2024-01-19 12:26:17 -0800

    Add files via upload
    Improved visualization

--- streamlit_localization_experiments.py
+++ streamlit_localization_experiments.py
@@ -4,37 +4,52 @@ from matplotlib import pyplot as plt # for visualization
 import geopy.distance # for calculating geographic distances
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 
 @st.cache_data
-def plotToA(hydlocs, longitudes, latitudes, lon_limits, lat_limits, toas):
+def plotIndividualProbs(hyd_lons, hyd_lats, longitudes, latitudes, lon_limits, lat_limits, toas):
     ## visualize the ToA
-    fig, axs = plt.subplots(1, len(hydlocs), figsize=(30, 5))
-    for h1 in range(positions.shape[0]):
+    fig, axs = plt.subplots(1, len(hyd_lons), figsize=(25, 5))
+    for h1 in range(len(hyd_lons)):
         pcol = axs[h1].pcolormesh(longitudes, latitudes, toas[h1].T, cmap='Blues_r', linewidth=0, rasterized=True, vmin=0, vmax=1)
         
-        for h2 in range(positions.shape[0]):
-            axs[h1].scatter(hydlocs[h2, 1], hydlocs[h2, 0], c='k', s=15)
-            axs[h1].text(hydlocs[h2, 1], hydlocs[h2, 0], h2, color='k', fontsize=15)
+        for h2 in range(len(hyd_lons)):
+            axs[h1].scatter(hyd_lons[h2], hyd_lats[h2], c='k', s=15)
+            axs[h1].text(hyd_lons[h2], hyd_lats[h2], h2, color='k', fontsize=15)
         
         axs[h1].set_xlim(lon_limits)
         axs[h1].set_ylim(lat_limits)
-        plt.colorbar(pcol)
+        #plt.colorbar(pcol)
     
     fig.tight_layout()
     return fig
 
-def plotProbabilityLandscape(positions, probs):
-    fig, axs = plt.subplots(1, 3, figsize=(21, 5))
-    pcol = axs[1].pcolormesh(longitudes, latitudes, probs.T, cmap='Blues_r', linewidth=0, rasterized=True, vmin=0, vmax=1)
+# def plotProbabilityLandscape(positions, probs):
+#     fig, axs = plt.subplots(1, 3, figsize=(21, 5))
+#     pcol = axs[1].pcolormesh(longitudes, latitudes, probs.T, cmap='Blues_r', linewidth=0, rasterized=True, vmin=0, vmax=1)
 
-    for h2 in range(positions.shape[0]):
-        axs[1].scatter(positions[h2, 1], positions[h2, 0], c='k', s=15)
-        axs[1].text(positions[h2, 1], positions[h2, 0], h2 + 1, color='k', fontsize=15)
+#     for h2 in range(len(positions)):
+#         axs[1].scatter(positions[h2, 'Longitude'], positions[h2, 0], c='k', s=15)
+#         axs[1].text(positions[h2, 1], positions[h2, 0], h2 + 1, color='k', fontsize=15)
 
-    axs[1].set_xlim(lon_limits)
-    axs[1].set_ylim(lat_limits)
-    axs[0].axis('off')
-    axs[2].axis('off')
+#     axs[1].set_xlim(lon_limits)
+#     axs[1].set_ylim(lat_limits)
+#     axs[0].axis('off')
+#     axs[2].axis('off')
+#     fig.colorbar(pcol)
+#     return fig
+
+def plotProbabilityLandscapeSingle(lons, lats, probs):
+    fig, axs = plt.subplots(1, 1, figsize=(7, 5))
+    pcol = axs.pcolormesh(longitudes, latitudes, probs.T, cmap='Blues_r', linewidth=0, rasterized=True, vmin=0, vmax=1)
+
+    for h2 in range(len(lons)):
+        axs.scatter(lons[h2], lats[h2], c='k', s=15)
+        axs.text(lons[h2], lats[h2], h2 + 1, color='k', fontsize=15)
+
+    axs.set_xlim(lon_limits)
+    axs.set_ylim(lat_limits)
     fig.colorbar(pcol)
     return fig
 
@@ -42,18 +57,19 @@ def plotProbabilityLandscape(positions, probs):
 
 ## ---- define all coordinates / parameters ---- ##
 
-st.subheader('Current lat/lon coordinates:')
+st.header('Visualizing the probability of detection across an array ~')
 
-positions = np.array([[41.933717, -70.2286],
-                      [41.9466, -70.40015],
-                      [41.8413, -70.469317],
-                      [41.839717, -70.3148],
-                      [41.8949, -70.359167]])
+st.text('Sample lat/lon coordinates:')
 
-num_units = positions.shape[0]
-print(positions.shape[0])
+lats = np.array([41.933717, 41.9466, 41.8413, 41.839717, 41.8949])
+lons = np.array([-70.2286, -70.40015, -70.469317, -70.3148, -70.359167])
 
-st.dataframe(pd.DataFrame(positions))
+positions = {'Latitude': lats, 'Longitude': lons}
+positions = pd.DataFrame(positions)
+
+num_units = len(positions)
+c0, c1, c2 = st.columns((4, 4, 2))
+c1.dataframe(positions)
 
 
 ## ---- define all coordinates / parameters ---- ##
@@ -63,12 +79,12 @@ st.dataframe(pd.DataFrame(positions))
 num_lon = 150
 num_lat = 150
 
-center_lon = np.mean(positions[:, 1])
-center_lat = np.mean(positions[:, 0])
+center_lon = np.mean(lons)
+center_lat = np.mean(lats)
 
 # these are the max/min limits for longitude and latitude
-lon_limits = [center_lon + 2 * (np.min(positions[:, 1]) - center_lon), center_lon + 2 * (np.max(positions[:, 1]) - center_lon)]
-lat_limits = [center_lat + 2 * (np.min(positions[:, 0]) - center_lat), center_lat + 2 * (np.max(positions[:, 0]) - center_lat)]
+lon_limits = [center_lon + 2 * (np.min(lons) - center_lon), center_lon + 2 * (np.max(lons) - center_lon)]
+lat_limits = [center_lat + 2 * (np.min(lats) - center_lat), center_lat + 2 * (np.max(lats) - center_lat)]
 
 # define somme parameters 
 speed_of_sound = 1.484 # estimated average speed of sound for Cape Cod Bay based on salinity/temperature
@@ -90,50 +106,54 @@ for hyd in range(num_units):
     for i in range(num_lat):
         for j in range(num_lon):
             distances[hyd][j, i] = geopy.distance.distance((latitudes[i], longitudes[j]),
-                                                      (positions[hyd, 0], positions[hyd, 1])).km
+                                                      (lats[hyd], lons[hyd])).km
 
 
-st.text('Current array geometry:')
-fig_array = plotProbabilityLandscape(positions, np.ones((num_lon, num_lat)))
-st.pyplot(fig_array)
+st.text('Sample array geometry:')
+fig_array = plotProbabilityLandscapeSingle(lons, lats, np.ones((num_lon, num_lat)))
+
+print(lons)
+print(lats)
+
+# make two columns
+c0, c1, c2 = st.columns((3, 4, 3))
+c1.pyplot(fig_array)
 
 
 ## ---- detection function ---- ##
 
 max_distance = np.max(distances[0])
 
-#st.text('Choose a detection function:')
-
 function_type = st.radio('Choose a detection function:', ['Half-normal', 'Hazard-rate'])
 
 t = np.linspace(0, max_distance, 1000)
 
 if (function_type == 'Half-normal'):
-    sd_parameter = st.slider('SD:', value=1.0, min_value=0.0, max_value = max_distance, step=0.01, format="%.3f")
+    sd_parameter = st.slider('SD:', value=5.0, min_value=0.0, max_value = max_distance, step=0.01, format="%.3f")
     det_function = lambda x: (np.exp(-x**(2) / (2 * sd_parameter**2)))
 else:
     c1, c2 = st.columns(2)
-    beta_parameter = c1.slider('Beta:', value=1.0, min_value=0.0, max_value = 10.0, step=0.01, format="%.3f")
-    sd_parameter = c2.slider('SD:', value=1.0, min_value=0.0, max_value = max_distance, step=0.01, format="%.3f")
+    beta_parameter = c1.slider('Exponent (beta):', value=1.0, min_value=0.0, max_value = 10.0, step=0.01, format="%.3f")
+    sd_parameter = c2.slider('SD:', value=5.0, min_value=0.0, max_value = max_distance, step=0.01, format="%.3f")
     det_function = lambda x: (1 - np.exp(-(x / sd_parameter)**(-beta_parameter)))
 
 
 # plot detection function
-fig_det_function, axs_det_function = plt.subplots(1, 3, figsize=(20, 5))
-axs_det_function[1].plot(t, det_function(t))
-axs_det_function[1].set_xlabel('Distance')
-axs_det_function[1].set_ylabel('Probability of Detection')
-axs_det_function[1].set_title('Detection Function')
-axs_det_function[0].axis('off')
-axs_det_function[2].axis('off')
-st.pyplot(fig_det_function)
+c0, c1, c2 = st.columns((3, 4, 3))
+fig_det_function, axs_det_function = plt.subplots(1, 1, figsize=(7, 5))
+axs_det_function.plot(t, det_function(t))
+axs_det_function.set_xlabel('Distance')
+axs_det_function.set_ylabel('Probability of Detection')
+axs_det_function.set_title('Detection Function')
+c1.pyplot(fig_det_function)
 
 probs = {i: det_function(distances[i]) for i in range(num_units)}
 
 # calculate time-difference-of-arrival for each hydrophone pair
 st.text('Probability of detection for each unit individually:')
-fig_toa = plotToA(positions, longitudes, latitudes, lon_limits, lat_limits, probs)
+fig_toa = plotIndividualProbs(lons, lats, longitudes, latitudes, lon_limits, lat_limits, probs)
 st.pyplot(fig_toa)
+
 
 ## ---- zero units ---- ##
 
@@ -143,8 +163,9 @@ no_detection = np.ones((num_lon, num_lat))
 for h1 in range(num_units):
     no_detection = no_detection * (1 - probs[h1])
 
-fig_no_detection = plotProbabilityLandscape(positions, no_detection)
-st.pyplot(fig_no_detection)
+fig_no_detection = plotProbabilityLandscapeSingle(lons, lats, no_detection)
+c0, c1, c2 = st.columns((3, 4, 3))
+c1.pyplot(fig_no_detection)
 
 
 ## ---- one unit ---- ##
@@ -167,8 +188,9 @@ for h1 in range(num_units):
     print('Maximum: %.2f' % np.max(current_product))
     detection_at_one_unit = detection_at_one_unit + current_product
 
-fig_det_at_one_unit = plotProbabilityLandscape(positions, detection_at_one_unit)
-st.pyplot(fig_det_at_one_unit)
+fig_det_at_one_unit = plotProbabilityLandscapeSingle(lons, lats, detection_at_one_unit)
+c0, c1, c2 = st.columns((3, 4, 3))
+c1.pyplot(fig_det_at_one_unit)
 
 
 ## ---- two units ---- ##
@@ -192,8 +214,9 @@ for h1 in range(num_units):
     print('Final maximum: %.2f' % np.max(current_product))
     detection_at_two_units = detection_at_two_units + current_product
 
-fig_det_at_two_units = plotProbabilityLandscape(positions, detection_at_two_units)
-st.pyplot(fig_det_at_two_units)
+fig_det_at_two_units = plotProbabilityLandscapeSingle(lons, lats, detection_at_two_units)
+c0, c1, c2 = st.columns((3, 4, 3))
+c1.pyplot(fig_det_at_two_units)
 
 
 
@@ -201,8 +224,9 @@ st.text('Probability that a call is detected at three or more units:')
 
 detection_at_three_or_more_units = np.ones((num_lon, num_lat)) - no_detection - detection_at_one_unit - detection_at_two_units
 
-fig_det_at_three_plus_units = plotProbabilityLandscape(positions, detection_at_three_or_more_units)
-st.pyplot(fig_det_at_three_plus_units)
+fig_det_at_three_plus_units = plotProbabilityLandscapeSingle(lons, lats, detection_at_three_or_more_units)
+c0, c1, c2 = st.columns((3, 4, 3))
+c1.pyplot(fig_det_at_three_plus_units)
 
 # LOCALIZATION DEMO
 
