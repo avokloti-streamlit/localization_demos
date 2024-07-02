67773d4ff6ddb1c9e807bfd2f889d96fa49e21ac
avokloti-streamlit <157151362+avokloti-streamlit@users.noreply.github.com>
2024-07-02 12:29:40 -0400

    Add files via upload
    Added map and translation/rotation to the app.

--- streamlit_localization_bowhead_census.py
+++ streamlit_localization_bowhead_census.py
@@ -1,9 +1,10 @@
 import numpy as np # for most calculations
 import pandas as pd # for working with tabular data
 from matplotlib import pyplot as plt # for visualization
-import geopy.distance # for calculating geographic distances
 import streamlit as st
 from Localizer import Localizer
+import geopandas as gpd
+from scipy.interpolate import interpn
 
 st.set_page_config(layout="wide")
 
@@ -34,7 +35,7 @@ def plotIndividualProbs(hyd_xs, hyd_ys, longitudes, latitudes, toas, limits_xs, 
 @st.cache_data
 def plotProbabilityLandscape(unit_xs, unit_ys, probs, study_xs, study_ys, limits_xs, limits_ys):
     fig, axs = plt.subplots(1, 1, figsize=(8, 4))
-    pcol = axs.pcolormesh(study_xs, study_ys, probs.T, cmap='Blues_r', linewidth=0, rasterized=True, vmin=0, vmax=1)
+    pcol = axs.pcolormesh(study_xs, study_ys, probs.T, cmap='magma', linewidth=0, rasterized=True, vmin=0, vmax=1)
 
     for h2 in range(len(unit_xs)):
         axs.scatter(unit_xs[h2], unit_ys[h2], c='k', s=15)
@@ -52,6 +53,9 @@ def plotProbabilityLandscape(unit_xs, unit_ys, probs, study_xs, study_ys, limits
     axs.set_aspect('equal', 'box')
     axs.set_ylim(limits_ys)
     axs.set_xlim(limits_xs)
+    axs.set_xlabel('Horizontal Axis (km)')
+    axs.set_ylabel('Vertical Axis (km)')
+    fig.tight_layout()
     fig.colorbar(pcol)
     return fig
 
@@ -215,19 +219,21 @@ def halfNormal(x, sd):
 
 ## ---- define all coordinates / parameters ---- ##
 
-st.header('Visualizing the probability of detection across an array:')
+st.header('Probability of Detection Visualization:')
 
-#st.text('Choose an array geometry:')
+z = 4
 
-geometries = {'Plus-sign': lambda d: (np.array([0, 0, 0, 0, 2 * d, d, -d, -2 * d]), np.array([5 * d, 4 * d, 2 * d, d, 3 * d, 3 * d, 3 * d, 3 * d])),
-              'Square Grid': lambda d: (np.array([-3/2 * d, -d/2, d/2, 3/2 * d, -3/2 * d, -d/2, d/2, 3/2 * d]), np.array([4, 4, 4, 4, 4 + d, 4 + d, 4 + d, 4 + d])),
-              'Triangular Grid': lambda d: (np.array([-d, 0, d, -d/2, d/2, -d, 0, d]), np.array([4, 4, 4, 4 + np.sqrt(3)/2 * d, 4 + np.sqrt(3)/2 * d, 4 + np.sqrt(3) * d, 4 + np.sqrt(3) * d, 4 + np.sqrt(3) * d])),
-              'Banner': lambda d: (np.array([-(1 + np.sqrt(3)/2) * d, -d, -d, 0, 0, d, d, (1 + np.sqrt(3)/2) * d]), np.array([4 + d/2, 4, 4 + d, 4, 4 + d, 4, 4 + d, 4 + d/2])),
+geometries = {'Horizontal Square Grid': lambda d: (np.array([-3/2 * d, -d/2, d/2, 3/2 * d, -3/2 * d, -d/2, d/2, 3/2 * d]), np.array([z, z, z, z, z + d, z + d, z + d, z + d])),
+              'Horizontal Triangular Grid': lambda d: (np.array([-d, 0, d, -d/2, d/2, -d, 0, d]), np.array([z, z, z, z + np.sqrt(3)/2 * d, z + np.sqrt(3)/2 * d, z + np.sqrt(3) * d, z + np.sqrt(3) * d, z + np.sqrt(3) * d])),
+              'Vertical Square Grid': lambda d: (np.array([-d/2, -d/2, -d/2, -d/2, d/2, d/2, d/2, d/2]), np.array([z, z + d, z + 2 * d, z + 3 * d, z, z + d, z + 2 * d, z + 3 * d])),
+              'Vertical Triangular Grid': lambda d: (np.array([-d, -d, -d, d, d, d, 0, 0]), np.array([z, z + d, z + 2 * d, z, z + d, z + 2 * d, z + d/2, z + d * 3/2])),
+              'Banner': lambda d: (np.array([-(1 + np.sqrt(3)/2) * d, -d, -d, 0, 0, d, d, (1 + np.sqrt(3)/2) * d]), np.array([z + d/2, z, d + z, z, d + z, z, d + z, z + d/2])),
               'Line': lambda d: (np.array([-7/2 * d, -5/2 * d, -3/2 * d, -d/2, d/2, d * 3/2, d * 5/2, d * 7/2]), 4 * np.ones(8)),
-              'Two Squares': lambda d: (np.array([-2 * d, -2 * d, -d, -d, d, d, 2 * d, 2 * d]), np.array([4, 4 + d, 4, 4 + d, 4, 4 + d, 4, 4 + d])),
+              'Two Squares': lambda d: (np.array([-2 * d, -2 * d, -d, -d, d, d, 2 * d, 2 * d]), np.array([z, z + d, z, z + d, z, z + d, z, z + d])),
+              'House': lambda d: (np.array([-d, -d, 0, 0, d, d, -d/2, d/2]), np.array([z, d + z, z, d + z, z, d + z, z + 2 * d, z + 2 * d]))
+              #'Plus-sign': lambda d: (np.array([0, 0, 0, 0, 2 * d, d, -d, -2 * d]), np.array([5 * d, 4 * d, 2 * d, d, 3 * d, 3 * d, 3 * d, 3 * d]))
               #'Triangular Grid': lambda d: (np.array([]), np.array([])),
               }
-
 
 empirical_sds = {'Ice-cover': {60: 17, 70: 12, 80: 4.5},
                  'Open-water': {60: 90, 70: 54, 80: 28}}
@@ -248,7 +254,7 @@ with st.sidebar:
     st.subheader('Array Parameters')
 
     array_choice = st.radio('Choose an array geometry:', list(geometries.keys()))
-    array_spacing = st.slider('Array Spacing:', value=5.0, min_value=0.0, max_value = 10.0, step=0.5, format="%.1f")
+    array_spacing = st.slider('Array Spacing:', value=5.0, min_value=0.0, max_value = 12.0, step=0.5, format="%.1f")
 
     unit_xs = geometries[array_choice](array_spacing)[0]
     unit_ys = geometries[array_choice](array_spacing)[1]
@@ -338,25 +344,132 @@ with st.expander('Probability that a call is detected at exactly two units:'):
     c0, c1, c2 = st.columns((2, 6, 2))
     c1.pyplot(fig_det_at_two_units)
 
+## ---- map ---- ##
+
+class FloatReader:
+    def __init__(self, filename):
+        self.f = open(filename, "rb")
+    
+    def read_floats(self, count : int):
+        return np.fromfile(self.f, dtype=np.float32, count=count, sep='')
+
+
+def rotateArray(x, y, theta, x_mean, y_mean):
+    theta = theta * np.pi/180
+    rotation = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+    output = rotation @ np.array([x, y])
+    return output[0] + x_mean, output[1] + y_mean
+
+@st.cache_data
+def loadMapData():
+    bathymetry = np.loadtxt('bathymetry.csv')
+    bathymetry = np.flipud(bathymetry)
+    bathymetry[bathymetry < -1000] = np.nan
+
+    xllcorner = 554458.24478533
+    yllcorner = 7893428.5169774
+    cellsize = 10
+    ncols = 5236
+    nrows = 4181
+
+    lons = np.linspace(xllcorner, xllcorner + cellsize * (ncols - 1), ncols//3)/1000
+    lats = np.linspace(yllcorner, yllcorner + cellsize * (nrows - 1), nrows//3)/1000
+
+    coast = gpd.read_file('data/individual_files_bb1_coast.shp')
+    crs_in_km = '+proj=utm +zone=4 +datum=WGS84 +units=km +no_defs +type=crs'
+    coast = coast.to_crs(crs_in_km)
+
+    perch_locations = pd.read_csv('Perch_Locations_Raw.csv')
+    perch_locations = perch_locations.drop(14)
+    perch_locations_gpd = gpd.points_from_xy(perch_locations['Lon'], perch_locations['Lat'], crs="EPSG:4326")
+    perch_locations_metric = perch_locations_gpd.to_crs(coast.crs)
+
+    return bathymetry, lons, lats, coast, perch_locations_metric
+
+
+def drawMap(bathymetry, lons, lats, coast, perch_locations_metric, x_array, y_array):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+    contours = plt.contourf(lons, lats, np.flipud(bathymetry), levels = np.arange(-140, 9, 10), cmap='Blues', zorder=2)
+    plt.contour(lons, lats, np.flipud(bathymetry), levels = [-75], cmap='cool', zorder=3)
+    # plt.pcolormesh(np.flipud(data[data > 0]), levels = np.arange(0, 50, 10), c='k')
+    #plt.pcolormesh(np.flipud(data_ground))
+    plt.colorbar(contours)
+    plt.scatter(x_mean, y_mean, s=60, color='red', marker='*', zorder=8)
+
+    for i, point in enumerate(perch_locations_metric):
+        plt.scatter(point.x, point.y, s=20, color='orange', marker='*', zorder=10)
+
+    for i in np.arange(8):
+        plt.scatter(x_array, y_array, s=15, c='k', zorder=10)
+
+    coast.plot(ax=ax, color = 'k', zorder=4)
+    ax.set_xlabel('Projected Longitude (km)')
+    ax.set_ylabel('Projected Latitude (km)')
+    return fig
+
+st.header('Map Visualization:')
+
+
+# prepare map information
+bathymetry, lons, lats, coast, perch_locations_metric = loadMapData()
+
+# mean longitude/latitude
+x_mean = np.mean([point.x for point in perch_locations_metric])
+y_mean = np.mean([point.y for point in perch_locations_metric])
+
+# user parameters for map
+c1, c2, c3 = st.columns(3)
+x_center = c1.number_input('Center longitude (km):', min_value=570.0, max_value=590.0, value=x_mean, step=1.0)
+y_center = c2.number_input('Center latitude (km):', min_value=7910.0, max_value=7930.0, value=y_mean, step=1.0)
+rotation = c3.number_input('Rotation (degrees):', min_value=-90, max_value=90, step=5, value=-35)
+
+# translate and rotate array
+x_array, y_array = rotateArray(unit_xs, unit_ys, rotation, x_center, y_center)
+
+# plot map
+fig_map = drawMap(bathymetry, lons, lats, coast, perch_locations_metric, x_array, y_array)
+st.pyplot(fig_map)
+
+
+st.header('Coordinates:')
+
+proposed_array_metric = gpd.points_from_xy(x_array, y_array, crs=coast.crs)
+proposed_array_epsg = proposed_array_metric.to_crs("EPSG:4326")
+
+depths = np.zeros(8)
+proposed_longitudes = np.zeros(8)
+proposed_latitudes = np.zeros(8)
+
+for i in range(8):
+    proposed_longitudes[i] = proposed_array_epsg[i].x
+    proposed_latitudes[i] = proposed_array_epsg[i].y
+    try:
+        depths[i] = np.round(interpn((lons, lats), np.flipud(bathymetry).T, (x_array[i], y_array[i])), 1)
+    except:
+        depths[i] = np.nan
+
+array = pd.DataFrame({'Longitude': proposed_longitudes, 'Latitude': proposed_latitudes, 'Depth': depths})
+st.dataframe(array)
+
 
 ## ---- spatial error ---- ##
 
-st.header('Visualizing spatial error across an array:')
+# st.header('Visualizing spatial error across an array:')
 
-with st.form("Error parameters"):
-    c1, c2, c3 = st.columns(3)
-    temporal_error = c1.number_input('Temporal measurement error:', min_value=0.0, max_value=5.0, value=0.01)
-    sd_parameter = c2.number_input('Temporal SD parameter:', min_value=0.0, max_value=5.0, value=0.1)
-    num_repeats = c3.number_input('Monte-Carlo runs:', min_value=0, max_value=100, step=1, value=5)
-    submitted = st.form_submit_button("Calculate!")
+# with st.form("Error parameters"):
+#     c1, c2, c3 = st.columns(3)
+#     temporal_error = c1.number_input('Temporal measurement error:', min_value=0.0, max_value=5.0, value=0.01)
+#     sd_parameter = c2.number_input('Temporal SD parameter:', min_value=0.0, max_value=5.0, value=0.1)
+#     num_repeats = c3.number_input('Monte-Carlo runs:', min_value=0, max_value=100, step=1, value=5)
+#     submitted = st.form_submit_button("Calculate!")
 
-# next, set a temporal error spatial deviation
-if submitted:
-    localizer = Localizer(unit_xs, unit_ys, study_xs, study_ys, speed_of_sound = 1.5, source_depths = [0])
+# # next, set a temporal error spatial deviation
+# if submitted:
+#     localizer = Localizer(unit_xs, unit_ys, study_xs, study_ys, speed_of_sound = 1.5, source_depths = [0])
 
-    errors_rms = calculateLocalizationError(localizer, temporal_error, sd_parameter, num_repeats)
-    print(np.min(errors_rms), np.max(errors_rms))
-    fig_error = plotErrorMap(unit_xs, unit_ys, errors_rms, localizer, limits_xs, limits_ys)
+#     errors_rms = calculateLocalizationError(localizer, temporal_error, sd_parameter, num_repeats)
+#     print(np.min(errors_rms), np.max(errors_rms))
+#     fig_error = plotErrorMap(unit_xs, unit_ys, errors_rms, localizer, limits_xs, limits_ys)
 
-    c0, c1, c2 = st.columns((2, 6, 2))
-    c1.pyplot(fig_error)
+#     c0, c1, c2 = st.columns((2, 6, 2))
+#     c1.pyplot(fig_error)
